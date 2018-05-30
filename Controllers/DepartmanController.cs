07708684397fe11149a1180e87561f2dbc2581ee
muhammed fatih,candan <mfatihcandan@gmail.com>
2018-05-30 19:19:55 +0300

    viewModel
    viewModel olarak ayarlandı

--- Controllers/DepartmanController.cs
+++ Controllers/DepartmanController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Models;
+using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers
 {
@@ -11,8 +12,19 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Detay(int id)
         {
-            ViewData["departmanId"] = id;
-            return View();
+            var departman = new Departman() { Id = id, Ad = "Mühendislik" };
+            var personeller = new List<Personel>()
+            {
+                new Personel(){ Ad="Personel1" },
+                new Personel(){ Ad="Personel2" },
+                new Personel(){ Ad="Personel3" }
+            };
+
+            var model = new DepartmanDetayViewModel() {
+                Departman = departman,
+                Personeller = personeller
+            };
+            return View(model);
         }
     }
 }
---
+++ ViewModels/DepartmanDetayViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewModels
+{
+    public class DepartmanDetayViewModel
+    {
+        public Departman Departman { get; set; }
+        public List<Personel> Personeller { get; set; }
+    }
+}
